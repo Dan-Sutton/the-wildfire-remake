edc2818b217d91c79fda1a74e4558f7dc2859406
Dan-Sutton <dewasutton@gmail.com>
2023-09-11 17:44:33 +0100

    favicon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "The Wildfire",
+  title: "The Wildfire | Function Band",
   description: "Four piece function band for hire.",
 };
 
